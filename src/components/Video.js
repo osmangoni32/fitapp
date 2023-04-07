81354743ca2471177cd613d526daa835a5d72c2c
unknown <osman.ice.32@gmail.com>
2023-04-07 10:34:21 +0600

    videos code edited

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,17 +1,15 @@
+import React from "react";
 import image from "../assets/images/3.jpg";
 import classes from "../styles/Video.module.css";
-import React from "react";
 export default function Video() {
   return (
-    <a href="quiz.html">
-      <div className={classes.video}>
-        <img src={image} alt="vt" />
-        <p>#23 React Hooks Bangla - React useReducer hook Bangla</p>
-        <div className={classes.qmeta}>
-          <p>10 Questions</p>
-          <p>Score : Not taken yet</p>
-        </div>
+    <div className={classes.video}>
+      <img src={image} alt="vt" />
+      <p>#23 React Hooks Bangla - React useReducer hook Bangla</p>
+      <div className={classes.qmeta}>
+        <p>10 Questions</p>
+        <p>Score : Not taken yet</p>
       </div>
-    </a>
+    </div>
   );
 }
