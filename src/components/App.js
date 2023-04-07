62e89961a5608e632d2285200964bc7cb5e1eb06
unknown <osman.ice.32@gmail.com>
2023-04-07 22:46:23 +0600

    preivateOutlet added

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "../styles/App.css";
 import { AuthProvider } from "./Context/AuthContext";
 import Layout from "./Layout";
+import PrivateOutlet from "./PrivateOutlet";
+import PublicOutlet from "./PublicOutlet";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Quiz from "./pages/Quiz";
@@ -17,10 +19,21 @@ function App() {
           <Routes>
             {/* <Route exact path="/" component={Home} /> */}
             <Route path="/" element={<Home />} />
-            <Route path="/signup" element={<Singup />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/quiz" element={<Quiz />} />
-            <Route path="/result" element={<Result />} />
+
+            <Route path="/signup" element={<PublicOutlet />}>
+              <Route path="/signup" element={<Singup />} />
+            </Route>
+            <Route path="/login" element={<PublicOutlet />}>
+              <Route path="/login" element={<Login />} />
+            </Route>
+            <Route path="/quiz" element={<PrivateOutlet />}>
+              <Route path="/quiz" element={<Quiz />} />
+            </Route>
+            <Route path="/result" element={<PrivateOutlet />}>
+              <Route path="/result" element={<Result />} />
+            </Route>
+
+            {/*  <Route path="/quiz" element={<Quiz />} /> */}
           </Routes>
         </Layout>
       </AuthProvider>
---
+++ src/components/PrivateOutlet.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Outlet,Navigate } from "react-router-dom";
+import {useAuth} from "./Context/AuthContext"
+
+
+export default function PrivateOutlet(){
+    const {currentUser} =useAuth();
+    return currentUser? <Outlet/>:<Navigate to="/login"/>
+}
